f555306b7df6ff34d6a3cdbccf880f3e6b53490f
anildalar <dollorinfotech@gmail.com>
2023-06-27 09:21:36 +0530

    new

--- js/script.js
+++ js/script.js
@@ -21,10 +21,20 @@ let logout = ()=>{
     window.location.reload();
 }
 
+let playSound = ()=>{
+    //console.log('Good morning');
+    let at = document.querySelector('.a_audio');
+    at.play();
+}
+
 // ()();   IIFE 
+//Onpage load
 (()=>{
+
     let fn = window.localStorage.getItem('first_name');
     let ln = window.localStorage.getItem('last_name');
+    
+
     console.log('Page loaded successfully');
     // Get a reference to the modal element
     var modal = document.getElementById('regitrationModel');
@@ -46,5 +56,38 @@ let logout = ()=>{
     }
 
 
+    //Keypress sound play
+    document.addEventListener('keypress',(e)=>{
+        console.log('->',e.keyCode);
+        if(e.keyCode == 97){
+            let a = document.querySelector('.a_a');
+            console.log(a.classList.add('a_active'));
+            document.querySelector('.l_pinky').style.display = 'block';
+        } 
+        if(e.keyCode == 113){
+            let a = document.querySelector('.a_q');
+            console.log(a.classList.add('a_active'));
+            document.querySelector('.l_pinky').style.display = 'block';
+        } 
+        if(e.keyCode == 122){
+            let a = document.querySelector('.a_z');
+            console.log(a.classList.add('a_active'));
+            document.querySelector('.l_pinky').style.display = 'block';
+        } 
+        //document = whole website
+        playSound();
+    })
 
+    document.addEventListener('keyup',(e)=>{
+        console.log(e);
+        var elements = document.getElementsByClassName('a_active');
+  
+        for (var i = 0; i < elements.length; i++) {
+          elements[i].classList.remove('a_active');
+        }
+        document.querySelector('.a_left_pinky').style.display='none';
+    });
+    
 })();
+
+//()(); IIFE 
