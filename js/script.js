228510bd6a9602ff7093d98fab6f4b68567f9727
anildalar <dollorinfotech@gmail.com>
2023-06-28 09:12:45 +0530

    timer done

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ let saveRegistrationInfo = ()=>{
             //object.method()
     let fn = document.getElementById("first_name").value;
     let ln = document.getElementById("last_name").value;
+    let dur = document.querySelector('.a_myselect').value;
 
     console.log(fn);
     console.log(ln);
     window.localStorage.setItem('first_name',fn);
     window.localStorage.setItem('last_name',ln);
+    window.localStorage.setItem('duration',dur);
      //Page reload
      window.location.reload();
 }
@@ -25,6 +27,43 @@ let playSound = ()=>{
     //console.log('Good morning');
     let at = document.querySelector('.a_audio');
     at.play();
+}
+
+let start = ()=>{
+    //Built-in function
+    // Set the date we're counting down to
+    var nextTime = new Date().getTime();
+    nextTime = new Date(nextTime + ( localStorage.getItem('duration')*60* 1000)); // Add 1 hour
+    
+        // Create a new Date object with the specified datetime
+    const datetime = new Date( nextTime);
+
+    // Get the Unix timestamp by dividing the milliseconds since January 1, 1970, by 1000
+    const unixTimestamp = Math.floor(datetime.getTime() / 1000)*1000;
+
+   
+    setInterval(function(){
+        // Get today's date and time
+        var now = new Date().getTime();
+        // Add a certain amount of time to the current time
+        console.log('Next Time', unixTimestamp);
+        console.log('Current Time',now);
+         // Find the distance between now and the count down date
+        var distance = unixTimestamp - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.querySelector(".a_myduration").innerHTML = minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.querySelector(".a_myduration").innerHTML = "EXPIRED";
+        }
+    },1000)
 }
 
 // ()();   IIFE 
@@ -110,7 +149,26 @@ let playSound = ()=>{
 
         //document.querySelector('.a_left_pinky').style.display='none';
     });
+
+    /*
+        for (expression 1; condition; increment++/decrement) {
+            // code block to be executed
+        }
+    */
+   var o = '';
+   for(var i=1; i<=60;i++){
+        o = o + `<option value="${i}">${i}</option>`;
+   }
+
+   console.log(o);
+   console.log(document.querySelector('.a_myselect'));
+   document.querySelector('.a_myselect').innerHTML = o;
     
+
+   document.querySelector('.a_myduration').innerHTML = localStorage.getItem('duration') === null ?'':localStorage.getItem('duration')+':00';
+
+    
+
 })();
 
 //()(); IIFE 
